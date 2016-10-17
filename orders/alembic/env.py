4411a5dcb7d658d779fbf09406aace50f25847ee
Jakub Borys <jakub.borys@gmail.com>
2016-10-17 09:55:50 +0100

    Alembic use env vars in online migrations

--- orders/alembic/env.py
+++ orders/alembic/env.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 import os
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine, pool
 from logging.config import fileConfig
 
 from orders.models import DeclarativeBase
@@ -67,10 +67,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+    connectable = create_engine(get_url())
 
     with connectable.connect() as connection:
         context.configure(
