e433850d100201cb4c12c0c13d34bf3130f25d48
timbu <tim.buckland@student.com>
2016-12-05 18:43:28 +0000

    more gateway sketching

---
+++ gateway/gateway/schemas.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields
+
+
+class CreateOrderDetailSchema(Schema):
+    product_id = fields.Str(required=True)
+    price = fields.Decimal(as_string=True, required=True)
+    quantity = fields.Int(required=True)
+
+
+class CreateOrderSchema(Schema):
+    order_details = fields.Nested(CreateOrderDetailSchema, many=True, required=True)
--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -1,68 +1,115 @@
 import json
+
+from marshmallow import ValidationError
 from nameko.rpc import RpcProxy
 from nameko.web.handlers import http
 from werkzeug import Response
 
-
-class OrderNotFound(Exception):
-    pass
+from gateway.dependencies import Config
+from gateway.exceptions import OrderNotFound, ProductNotFound
+from gateway.schemas import CreateOrderSchema
 
 
 class Gateway(object):
     """
-    Service acts as a gateway to allow access to our other service over http.
+    Service acts as a gateway to other services over http.
     """
+
     name = 'gateway'
 
-    # TODO - config dependency
-    # TODO - schemas?
-
+    config = Config()
     orders_rpc = RpcProxy('orders')
     products_rpc = RpcProxy('products')
 
-    @http('GET', '/orders')
-    def list_orders(self, request):
-        """
-        List all orders in the system.
-        """
-        orders = self.orders_rpc.list_orders()
-
-        return Response(json.dumps(orders), mimetype='application/json')
-
-    @http("GET", "/orders/<int:order_id>", expected_exceptions=OrderNotFound)
+    @http("GET", "/orders/<int:order_id>")
     def get_order(self, request, order_id):
+        """Gets the order details for the order given by `order_id`.
+
+        Enhances the order details with full product details from the
+        products-service.
         """
-        Get the order details for order given by `order_id`.
-        Also asynchronously collects product details for the order from the
-        product service.
-        """
-        order = self.orders_rpc.get_order(order_id)
-        if not order:
-            # TODO maybe should make sure we return a 404?
-            raise OrderNotFound('Order id {}'.format(order_id))
-
-        # make async calls to products service.
-        product_results = [
-            self.products_rpc.get_product.call_async(product_id)
-            for product_id in order['product_ids']
-        ]
-
-        # modify the order dict to include product details
-        order['products'] = [result.result() for result in product_results]
-
-        # TODO - prepend configured domain name to product images.
+        try:
+            order = self._get_order(order_id)
+        except OrderNotFound:
+            # Return 404
+            return Response("Order {} not found".format(order_id), status=404)
+        except ProductNotFound as exc:
+            # Return a 500
+            return Response(
+                "Order {} is invalid: "
+                "Product {} not found.".format(order_id, exc),
+                status=500
+            )
 
         return Response(json.dumps(order), mimetype='application/json')
+
+    def _get_order(self, order_id):
+        # Retrieve order data from the orders service.
+        # Note - this may raise a remote exception that has been mapped to
+        # raise``OrderNotFound``
+        order = self.orders_rpc.get_order(order_id)
+
+        # Retrieve all products from the products service
+        product_map = {prod['id']: prod for prod in self.products_rpc.list()}
+
+        # get the configured image root
+        image_root = self.config['PRODUCT_IMAGE_ROOT']
+
+        # Enhance order details with product and image details.
+        for item in order['order_details']:
+            product_id = item['product_id']
+            try:
+                item['product'] = product_map[product_id]
+            except KeyError:
+                raise ProductNotFound("Product Id {}".format(product_id))
+
+            # Construct an image url.
+            item['image'] = '{}/{}.jpg'.format(image_root, product_id)
+
+        return order
 
     @http("POST", "/orders")
     def create_order(self, request):
         """
         Create a new order - order data is posted as json
         """
-        order_data = json.loads(request.get_data(as_text=True))
+        try:
+            raw_data = json.loads(request.get_data(as_text=True))
+        except ValueError as exc:
+            # Return 400
+            return Response(
+                "Bad request - Invalid json: {}".format(exc), status=400
+            )
 
-        # some data validation would be needed.
+        try:
+            # load input data through a schema (for validation)
+            schema = CreateOrderSchema(strict=True)
+            order_data = schema.load(raw_data).data
+        except ValidationError as exc:
+            # Return 400
+            return Response("Bad request: {}".format(exc), status=400)
 
-        order_result = self.rpc.orders.create_order(order_data)
+        try:
+            id_ = self._create_order(order_data)
+        except ProductNotFound as exc:
+            return Response("ProductNotFound: {}".format(exc), status=404)
 
-        return Response(json.dumps(order_result), mimetype='application/json')
+        return Response(json.dumps({'id': id_}), mimetype='application/json')
+
+    def _create_order(self, order_data):
+        # check order product ids are valid
+        valid_product_ids = {prod['id'] for prod in self.products_rpc.list()}
+        for item in order_data['order_details']:
+            if item['product_id'] not in valid_product_ids:
+                raise ProductNotFound(
+                    "Product Id {}".format(item['product_id'])
+                )
+
+        # Call orders-service to create the order.
+        # Dump the data through the schema to ensure the values are serialized
+        # correctly.
+        serialized_data = CreateOrderSchema().dump(order_data).data
+        result = self.orders_rpc.create_order(
+            serialized_data['order_details']
+        )
+        return result['id']
